99fe2150b270c058e0137102f226b6bdc5ba2030
Nigel <usipapers@gmail.com>
2022-01-20 15:21:00 -0500

    first commit

---
+++ tax/script.py
@@ -0,0 +1,12 @@
+name = 'Monty Python'
+description = 'sketch comedy group'
+year = 1969
+
+# Introduce them
+sentence = name + ' is a ' + description + ' formed in ' + str(year)
+print(sentence)
+
+# Describe Monty Python's work
+famous_sketch1 = "\n\tHell's Grannies"
+famous_sketch2 = '\n\tThe Dead Parrot'
+print('Famous Work:', famous_sketch1, famous_sketch2)
---
+++ timeCalculator/calculator.py
@@ -0,0 +1,44 @@
+
+
+time = {}
+
+def ask_hours():
+    response_hours = input("How many hours?")
+    time['hours'] = float(response_hours)
+ask_hours()
+
+def ask_minutes():
+    response_minutes = input("And how many minutes?")
+    time['minutes'] = float(response_minutes)
+ask_minutes()
+
+def ask_speed():
+    response_speed = input("At which speed are you watching the course? Please respond in decimal format, (example: 0.6 or 1.4)")
+    time['speed'] = float(response_speed)
+ask_speed()
+
+import math
+def time_calulator(time):
+    
+    speed = time.get('speed')
+    
+    hours = time.get('hours')
+    
+    minutes = time.get('minutes')
+    
+    total_minutes = (hours * 60) + minutes
+
+    find_change_of_time = speed * total_minutes
+
+    if speed < 1:
+        add_time = find_change_of_time + total_minutes
+        new_hours = add_time / 60
+        new_minutes = math.modf(new_hours)[0] * 60
+        new_seconds = int(math.modf(new_minutes)[0] * 60)
+        print("Your new runtime is " + str(math.modf(new_hours)[1]) + " hours, "
+        + str(math.modf(new_minutes)[1]) + " minutes, and " + str(new_seconds) + " seconds.")
+    else:
+        subtracted_time = total_minutes - find_change_of_time
+        print(subtracted_time)
+
+time_calulator(time)
